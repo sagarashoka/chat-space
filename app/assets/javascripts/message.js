46658886e3f63c619b2ce95073ef958a54d3f71c
shokasagara <shoka.0701@gmail.com>
2020-07-21 00:16:07 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message">
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message">
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main_chat_2').append(html);
+      $('form')[0].reset();
+      $('.main_chat_2').animate({ scrollTop: $('.main_chat_2')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
+      $(".submit-btn").removeAttr("disabled")
+    });
+  });
+});
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -16,4 +17,5 @@ module ChatSpace
     end
     config.i18n.default_locale = :ja    
   end
+
 end
