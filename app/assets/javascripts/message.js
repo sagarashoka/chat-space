ff31068fc6de8717200d365a3e0494341a667954
shokasagara <shoka.0701@gmail.com>
2020-07-27 20:27:44 +0900

    Revert "Revert "Merge pull request #7 from sagarashoka/自動更新""
    This reverts commit 606acbbe04ed8c4a7f72151636e856d81e9b60b9.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message" data-message-id=${message.id}>
+      `<div class="message">
          <div class="message__upper-info">
            <div class="message__upper-info__talker">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message" data-message-id=${message.id}>
+      `<div class="message">
          <div class="message__upper-info">
            <div class="message__upper-info__talker">
              ${message.user_name}
@@ -52,7 +52,6 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data)
       var html = buildHTML(data);
       $('.main_chat_2').append(html);
       $('form')[0].reset();
@@ -64,32 +63,5 @@ $(function(){
     .always(function(){
       $(".submit-btn").removeAttr("disabled")
     });
-  })
-
-  var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id )
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.main_chat_2').append(insertHTML);
-        $('.main_chat_2').animate({ scrollTop: $('.main_chat_2')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) { 
-    setInterval(reloadMessages, 7000);
-  }
+  });
 });
